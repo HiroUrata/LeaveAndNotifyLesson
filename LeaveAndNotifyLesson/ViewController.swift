7919eff36f357ad922c9dd6e341961692da15795
HiroUrata <1712026@i.softbank.jp>
2021-11-24 22:36:34 +0900

    DispatchGroup .leave()と.notify()　復習コード追加

--- LeaveAndNotifyLesson/ViewController.swift
+++ LeaveAndNotifyLesson/ViewController.swift
@@ -11,9 +11,34 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let dispatchGroup = DispatchGroup()
+        let dispatchQueue = DispatchQueue(label: "test",attributes: .concurrent)
+        
+        dispatchGroup.enter()
+        dispatchQueue.async {
+            
+            print("testMove開始")
+            self.testMove()
+            dispatchGroup.leave()
+        }
+        
+        dispatchGroup.notify(queue: .main){
+            
+            print("testMove終了")
+        }
     }
 
 
 }
 
+extension ViewController{
+    
+    private func testMove(){
+        
+        for count in 0...20{
+            
+            print(String(count))
+        }
+    }
+}
