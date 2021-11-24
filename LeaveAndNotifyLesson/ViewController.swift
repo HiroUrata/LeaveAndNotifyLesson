b990e7c71a92a752c83a587daa55d8fb4d000c75
HiroUrata <1712026@i.softbank.jp>
2021-11-24 21:33:25 +0900

    Initial Commit

---
+++ LeaveAndNotifyLesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeaveAndNotifyLesson
+//
+//  Created by UrataHiroki on 2021/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
